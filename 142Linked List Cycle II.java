a4a4a2ab1c936b6aafcc8475010eeb1efa5ced31
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2022-03-30 09:35:04 +0530

    Add files via upload

---
+++ 142Linked List Cycle II.java
@@ -0,0 +1,27 @@
+//this is floyd algorithm for cycle detection also called as tortoise and hare method
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode curr=cycle(head);
+        if(curr==null || curr.next==null)return null;//this is to check if cycle is found or not
+        ListNode run=head;
+        while(curr!=run){
+            run=run.next;
+            curr=curr.next;
+        }
+        return run;    
+    }
+    public ListNode cycle(ListNode head){
+          if (head == null || head.next == null)
+             return null;
+         ListNode fast=head;
+         ListNode slow=head;
+         while(fast!=null && fast.next!=null ){
+             fast=fast.next.next;
+             slow=slow.next;
+             if(fast==slow){
+                return slow; 
+             } 
+         }
+         return null;
+    }
+}
