598747dc5ecb760f842bd02b3fed62f5254e51e3
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2022-10-26 12:14:06 +0530

    Time: 1132 ms (5.00%), Space: 163.1 MB (5.04%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -1,24 +1,56 @@
 class Solution {
     public int maxLength(List<String> arr) {
-         if (arr == null || arr.size() == 0) return 0;
-        int[] max = new int[1];
-        dfs("", 0, arr, max);
-        return max[0];
+        //  if (arr == null || arr.size() == 0) return 0;
+        // int[] max = new int[1];
+        // dfs("", 0, arr, max);
+        // return max[0];
+        return solve(arr,0,"");
     }
-   public void dfs(String path, int index, List<String> arr, int[] max) {
-        if (!isUniqueChars(path)) return;
+//    public void dfs(String path, int index, List<String> arr, int[] max) {
+//         if (!isUniqueChars(path)) return;
         
-        max[0] = Math.max(max[0], path.length());
-        if (index == arr.size()) return;
+//         max[0] = Math.max(max[0], path.length());
+//         if (index == arr.size()) return;
         
-        // for each string in arr
-        for (int i = index; i < arr.size(); i++) {
-            if (!isUniqueChars(arr.get(i))) continue;
-            // try subsequent strings in arr
-            dfs(path + arr.get(i), i+1, arr, max);
+//         // for each string in arr
+//         for (int i = index; i < arr.size(); i++) {
+//             if (!isUniqueChars(arr.get(i))) continue;
+//             // try subsequent strings in arr
+//             dfs(path + arr.get(i), i+1, arr, max);
+//         }
+//     }
+//     public boolean isUniqueChars(String s){
+//          Set<Character> set = new HashSet<>();
+
+//         for (char c : s.toCharArray()) {
+//             if (set.contains(c)) {
+//                 return false;
+//             }
+//             set.add(c);
+//         }
+//         return true;
+//     }
+    
+// TAKE OR NOT TAKE CONCEPT
+      public int solve(List<String> arr, int i, String soFar)
+    {
+        if(i == arr.size())
+        {
+            if(isValid(soFar))
+            {
+                return soFar.length();
+            }
+            return 0;
         }
+        
+        int size1 = solve(arr, i+1, soFar);
+        int size2 = solve(arr, i+1, soFar+arr.get(i));
+        
+        return Math.max(size1, size2);
+
     }
-    public boolean isUniqueChars(String s){
+    
+     public boolean isValid(String s){
          Set<Character> set = new HashSet<>();
 
         for (char c : s.toCharArray()) {
