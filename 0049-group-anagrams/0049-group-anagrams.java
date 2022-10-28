159056d8961e73009498fcfd06ab102a8029374b
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2022-10-29 01:17:41 +0530

    Time: 14 ms (62.82%), Space: 55.1 MB (80.41%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,51 +1,60 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        
+        Map<String,List<String>> map=new HashMap<>();
+        for(String s: strs){
+            char[] ch=s.toCharArray();
+            Arrays.sort(ch);
+            String key=new String(ch);
+            if(!map.containsKey(key))
+                map.put(key,new ArrayList<>());
+            map.get(key).add(s);
+        }
+        return new ArrayList<>(map.values());
         
         //brute force
-        List<List<String>> ans = new ArrayList<>();
-        if (strs == null || strs.length == 0) {
-            return ans;
-        }
+//         List<List<String>> ans = new ArrayList<>();
+//         if (strs == null || strs.length == 0) {
+//             return ans;
+//         }
 
-        int len = strs.length;
-        boolean[] visited = new boolean[len];
-        for (int i = 0; i < len; i++) {
-            if (visited[i]) {
-                continue;
-            }
+//         int len = strs.length;
+//         boolean[] visited = new boolean[len];
+//         for (int i = 0; i < len; i++) {
+//             if (visited[i]) {
+//                 continue;
+//             }
 
-            List<String> list = new ArrayList<>();
-            String word = strs[i];
-            list.add(word);
-            visited[i] = true;
+//             List<String> list = new ArrayList<>();
+//             String word = strs[i];
+//             list.add(word);
+//             visited[i] = true;
             
-            for (int j = i + 1; j < len; j++) {
-                if (checkAnagram(word, strs[j]) && !visited[j]) {
-                    list.add(strs[j]);
-                    visited[j] = true;
-                }
-            }
+//             for (int j = i + 1; j < len; j++) {
+//                 if (checkAnagram(word, strs[j]) && !visited[j]) {
+//                     list.add(strs[j]);
+//                     visited[j] = true;
+//                 }
+//             }
 
-            ans.add(list);
-        }
+//             ans.add(list);
+//         }
 
-        return ans;
+//         return ans;
     }
-    public boolean checkAnagram(String s, String t) {
-     if(s.length()!=t.length()){
-            return false;
-        }
-        int[] count = new int[26];
-        for(int i=0;i<s.length();i++){
-            count[s.charAt(i)-'a']++;
-            count[t.charAt(i)-'a']--;
-        }
-        for(int i:count){
-            if(i!=0){
-                return false;
-            }
-        }
-        return true;
-    }
+    // public boolean checkAnagram(String s, String t) {
+    //  if(s.length()!=t.length()){
+    //         return false;
+    //     }
+    //     int[] count = new int[26];
+    //     for(int i=0;i<s.length();i++){
+    //         count[s.charAt(i)-'a']++;
+    //         count[t.charAt(i)-'a']--;
+    //     }
+    //     for(int i:count){
+    //         if(i!=0){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
 }
