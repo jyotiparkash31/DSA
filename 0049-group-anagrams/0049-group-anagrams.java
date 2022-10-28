203aae2aa7a1c25d432f598529747df88d1ec89e
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2022-10-29 00:59:04 +0530

    Time: 695 ms (5.01%), Space: 44.6 MB (99.97%) - LeetHub

---
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -0,0 +1,51 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        
+        
+        //brute force
+        List<List<String>> ans = new ArrayList<>();
+        if (strs == null || strs.length == 0) {
+            return ans;
+        }
+
+        int len = strs.length;
+        boolean[] visited = new boolean[len];
+        for (int i = 0; i < len; i++) {
+            if (visited[i]) {
+                continue;
+            }
+
+            List<String> list = new ArrayList<>();
+            String word = strs[i];
+            list.add(word);
+            visited[i] = true;
+            
+            for (int j = i + 1; j < len; j++) {
+                if (checkAnagram(word, strs[j]) && !visited[j]) {
+                    list.add(strs[j]);
+                    visited[j] = true;
+                }
+            }
+
+            ans.add(list);
+        }
+
+        return ans;
+    }
+    public boolean checkAnagram(String s, String t) {
+     if(s.length()!=t.length()){
+            return false;
+        }
+        int[] count = new int[26];
+        for(int i=0;i<s.length();i++){
+            count[s.charAt(i)-'a']++;
+            count[t.charAt(i)-'a']--;
+        }
+        for(int i:count){
+            if(i!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
