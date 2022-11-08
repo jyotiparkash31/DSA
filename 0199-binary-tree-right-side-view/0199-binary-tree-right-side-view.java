5aabcca243e9493900c760f3342b937736477169
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2022-11-09 00:47:36 +0530

    Time: 1 ms (94.05%), Space: 42.6 MB (56.53%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -15,25 +15,40 @@
  */
 class Solution {
     public List<Integer> rightSideView(TreeNode root) {
-         List<Integer>ans=new ArrayList<>();
-        Queue<TreeNode> q=new LinkedList<>();//using queue as it is bfs traversal
-        if(root==null)return ans;
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size();//size for running loop to add the child   
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();//taking out the node from the queue
-                if(i==size-1){
-                    ans.add(node.val);
-                }
-                if(node.left!=null){//taking the left child in the queue
-                    q.offer(node.left);
-                }
-                 if(node.right!=null){//taking the right child in the queue
-                    q.offer(node.right);
-                }
-            }
-       }
-        return ans;
+    //      List<Integer>ans=new ArrayList<>();
+    //     Queue<TreeNode> q=new LinkedList<>();//using queue as it is bfs traversal
+    //     if(root==null)return ans;
+    //     q.offer(root);
+    //     while(!q.isEmpty()){
+    //         int size=q.size();//size for running loop to add the child   
+    //         for(int i=0;i<size;i++){
+    //             TreeNode node=q.poll();//taking out the node from the queue
+    //             if(i==size-1){
+    //                 ans.add(node.val);
+    //             }
+    //             if(node.left!=null){//taking the left child in the queue
+    //                 q.offer(node.left);
+    //             }
+    //              if(node.right!=null){//taking the right child in the queue
+    //                 q.offer(node.right);
+    //             }
+    //         }
+    //    }
+    //     return ans;
+        
+        List<Integer> list=new ArrayList<>();
+        help(root,list,0);
+        return list;
+        
+    }
+    public void help(TreeNode node,List<Integer> list,int level){
+        if(node==null)return ;
+        
+        if(level==list.size()){
+            list.add(node.val);
+        }
+        help(node.right,list,level+1);
+        help(node.left,list,level+1);
+ 
     }
 }
