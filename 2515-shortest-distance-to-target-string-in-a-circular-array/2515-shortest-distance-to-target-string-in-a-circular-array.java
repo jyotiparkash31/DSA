ee90d42be3f4ee84b2eac086655c5dce0d86e5fe
jyotiparkash31 <85404503+jyotiparkash31@users.noreply.github.com>
2023-02-11 22:31:22 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (83.79%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -1,27 +1,39 @@
 class Solution {
     public int closetTarget(String[] words, String target, int s) {
-        int dis=0;
-        int n=words.length;
-        Boolean[] visited=new Boolean[n];
-        for(int i=0;i<n;i++){
-            visited[i]=false;
-        }
-        System.out.println(visited[0]);
-        int r=s;
-        int l=s;
-        while(!visited[l] && !visited[r]){
-            if(words[l].equals(target) || words[r].equals(target)){
-                return dis;
-            }
-            visited[l]=true;
-            visited[r]=true;
-            dis++;
-            l--;
-            r++;
-            if(l==-1)l=n-1;
+//         int dis=0;
+//         int n=words.length;
+//         Boolean[] visited=new Boolean[n];
+//         for(int i=0;i<n;i++){
+//             visited[i]=false;
+//         }
+//         System.out.println(visited[0]);
+//         int r=s;
+//         int l=s;
+//         while(!visited[l] && !visited[r]){
+//             if(words[l].equals(target) || words[r].equals(target)){
+//                 return dis;
+//             }
+//             visited[l]=true;
+//             visited[r]=true;
+//             dis++;
+//             l--;
+//             r++;
+//             if(l==-1)l=n-1;
             
-            if(r==n)r=0;
+//             if(r==n)r=0;
+//         }
+//        return -1;
+        
+        int ans = Integer.MAX_VALUE;
+
+        for (int i = 0; i < words.length; i++) {
+            if (words[i].equals(target)) {
+                int dist = Math.abs(i - s);
+                int oppDist = words.length - dist;
+                ans = Math.min(ans, Math.min(dist, oppDist));
+            }
         }
-       return -1;
+
+        return ans == Integer.MAX_VALUE? -1: ans;
     }
 }
