b6d908cb56d0dbcad4823e61e0e4916bd2e51a81
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2023-09-05 14:04:00 +0530

    Time: 1 ms (99.94%), Space: 41.3 MB (54.82%) - LeetHub

--- 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.java
+++ 2844-minimum-operations-to-make-a-special-number/2844-minimum-operations-to-make-a-special-number.java
@@ -5,12 +5,17 @@ class Solution {
         while(j>=0){
             int i=j-1;
             while(i>=0){
-            int d=(num.charAt(i)-'0')*10+num.charAt(j)-'0';
+            if(num.charAt(j)-'0'==0 ||num.charAt(j)-'0'==5){
+                int d=(num.charAt(i)-'0')*10+num.charAt(j)-'0';
             if(d%25==0){
                 ans=Math.min(ans,n-i-2);
                 break;
             }
                 i--;  
+            }else{
+                break;
+            }
+            
         }
             if (num.charAt(j) == '0') ans = Math.min(ans, n - 1);
              j--;
