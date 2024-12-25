ee8f8c5945e9122d452b63874c20b7e244278ff6
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2024-12-26 00:29:03 +0530

    Time: 3 ms (38.00%), Space: 44.8 MB (20.19%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.java
@@ -19,6 +19,12 @@ class Solution {
         return helper(preorder,inorder,0,n-1,0);
         
     }
+// The basic idea is here:
+// Say we have 2 arrays, PRE and IN.
+// Preorder traversing implies that PRE[0] is the root node.
+// Then we can find this PRE[0] in IN, say it's IN[5].
+// Now we know that IN[5] is root, so we know that IN[0] - IN[4] is on the left side, IN[6] to the end is on the right side.
+// Recursively doing this on subarrays, we can build a tree out of it :)
     public TreeNode helper(int[] preorder, int[] inorder,int start,int end,int preidx){
         if(start>end)
             return null;
@@ -34,7 +40,7 @@ class Solution {
         TreeNode root=new TreeNode(nodeVal);
         root.left=helper(preorder,inorder,start,i-1,preidx+1);
         root.right=helper(preorder,inorder,i+1,end,preidx+i-start+1);
-        
+        //here preidx+i-start+1 means (i - Start) is the size of root's left subtree.
         return root;
     }
 }
