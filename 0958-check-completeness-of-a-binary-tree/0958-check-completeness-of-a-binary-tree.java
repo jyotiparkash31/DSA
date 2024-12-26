61c738b7c5912680126ea516b015454b99d1cfb1
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2024-12-27 00:35:42 +0530

    Time: 1 ms (87.11%), Space: 42.6 MB (11.06%) - LeetHub

--- 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
+++ 0958-check-completeness-of-a-binary-tree/0958-check-completeness-of-a-binary-tree.java
@@ -17,6 +17,7 @@ class Solution {
     public boolean isCompleteTree(TreeNode root) {
         
         //this solution is using bfs or level order traversal
+        //this can be solved by dfs by using simple concept that index of a binary tree should not be grater than number of nodes in bt and index=for left->2*i, right->2*i+1
         Queue<TreeNode> qt=new LinkedList<>();
         boolean past=false;
         qt.offer(root);
