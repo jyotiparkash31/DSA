cc9a6ece49370f85081ddc12594bcd11c83232ce
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2024-12-28 20:16:15 +0530

    Time: 0 ms (100%), Space: 41.2 MB (49.3%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruningThis sol is o(n) its a dfs sol usinf recursive nature of tree.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    //we are checking that like in 2nd example first we are checking if 1 left has no 1 in it so we delete that portion and simminarly in right we check after going to each node and checking and this solution is o^n2 as first we are checking left and right side of a tree and if we find a one then we are are going to each node in left and right to check if one is there or not and delete that node
+    public TreeNode pruneTree(TreeNode root) {
+        if(root==null){
+            return null;
+        }
+        if(!isOnePresent(root.left)){
+             root.left=null;
+        }
+        if(!isOnePresent(root.right)){
+            root.right=null;
+        }
+        pruneTree(root.left);
+        pruneTree(root.right);
+
+        if(root.left==null && root.right==null && root.val==0){
+            return null;
+        }
+        return root;
+    }
+    //this method is to find if 1 is present in left or right portion of a root
+    public boolean isOnePresent(TreeNode root){
+        if(root==null){
+            return false;
+        }
+        if(root.val==1){
+            return true;
+        }
+        return isOnePresent(root.left)|| isOnePresent(root.right);        
+    }
+}
