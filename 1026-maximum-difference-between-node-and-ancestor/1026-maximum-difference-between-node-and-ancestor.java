05b09f2f598383404b2c296719b1230d667668c2
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2024-12-31 21:41:26 +0530

    Time: 0 ms (100%), Space: 41.6 MB (97.05%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -14,20 +14,46 @@
  * }
  */
 class Solution {
+    //int maximum=-1;
     public int maxAncestorDiff(TreeNode root) {
-        if(root==null)return 0;
-        return solve(root,root.val,100000);
+       if(root==null){
+        return 0;
+       }
+        return findMaxDiff(root,root.val,root.val);
     }
-    public int solve(TreeNode node,int currMax,int currMin){
-        if(node==null){
-            return currMax-currMin;
-        }
-        //the code will first return maxdiff from each leftsub tree and then right we have to return  overall maxdiff of the tree
-        currMax=Math.max(currMax,node.val);
-        currMin=Math.min(currMin,node.val);
-        int left=solve(node.left,currMax,currMin);
-        int right=solve(node.right,currMax,currMin);
-        
-        return Math.max(left,right);
+    //here we are finding max and min in every branch and at the end of a branch we are returning the diff and we are comparing the lef each branch diff and taking the max and returning
+public int findMaxDiff(TreeNode node,int min,int max){
+    if(node==null){
+        return Math.abs(max-min);
     }
+
+    min=Math.min(min,node.val);
+    max=Math.max(max,node.val);
+
+    int l=findMaxDiff(node.left,min,max);
+    int r=findMaxDiff(node.right,min,max);
+
+    return Math.max(l,r);
+}
+
+    //This is a brute force solution in which we are taking one node at a time and comparing the diff with the beow child form R and L and again doing down to its child left and Right and doing the same for that child 
+    // public void maxDiff(TreeNode node){
+    //     if(node==null){
+    //         return;
+    //     }
+    //     max(node,node.left);
+    //     max(node,node.right);
+
+    //     maxDiff(node.left);
+    //     maxDiff(node.right);
+    // }
+    // public void max(TreeNode root,TreeNode child){
+    //     if(root==null || child==null){
+    //         return ;
+    //     }
+    //     maximum=Math.max(maximum,Math.abs(root.val-child.val));
+         
+    //     max(root,child.left);
+    //     max(root,child.right);
+    // }
 }
