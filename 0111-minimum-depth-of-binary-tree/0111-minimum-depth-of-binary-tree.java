17d9cc614e191ab0117011b55efd7f90bc4617e7
Jyoti Parkash <85404503+jyotiparkash31@users.noreply.github.com>
2025-01-05 14:08:08 +0530

    Time: 4 ms (75.54%), Space: 63.6 MB (14.94%) - LeetHub

--- 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
+++ 0111-minimum-depth-of-binary-tree/0111-minimum-depth-of-binary-tree.java
@@ -15,33 +15,18 @@
  */
 class Solution {
     public int minDepth(TreeNode root) {
-        //dfs code
-        if(root==null)return 0;
-        // if(root.left==null && root.right==null)return 1;
-        // if(root.left==null)return 1+ minDepth(root.right);
-        // if(root.right==null)return 1+ minDepth(root.left);
-        // return 1+ Math.min(minDepth(root.left),minDepth(root.right));
-        
-        //bfs code
-        
-        Queue<TreeNode> q=new LinkedList<>();//using queue as it is bfs traversal
-        int level=1;
-        q.offer(root);
-        while(!q.isEmpty()){
-            int size=q.size();//size for running loop to add the child
-            for(int i=0;i<size;i++){
-                TreeNode node=q.poll();//taking out the node from the queue
-                if(node.left==null && node.right==null)return level;
-                if(node.left!=null){//taking the left child in the queue
-                    q.offer(node.left);
-                }
-                 if(node.right!=null){//taking the right child in the queue
-                    q.offer(node.right);
-                }
-            }
-            level++;
-            
+        if(root==null){
+            return 0;
         }
-       return level;
+        //if bot the left and right child is null return 1 end of one branch;
+        if(root.left==null && root.right==null){
+            return 1;
+        }
+    // we check left branch and return the left branch depth and same with right branch
+        int l=root.left!=null?minDepth(root.left):Integer.MAX_VALUE;
+        int r=root.right!=null?minDepth(root.right):Integer.MAX_VALUE;
+        //finally we return min of both right and left branch
+        return 1+ Math.min(l,r);
+
     }
 }
